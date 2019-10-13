74e4d6097ba1f3a5e824d6f1c76e8af96fb50770
Alex <haragas6@gmail.com>
2019-10-13 09:53:40 +0300

    w/ gitmore

---
+++ cmake-build-debug/CMakeFiles/chatAbot.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/chatAbot.dir/main.c.obj"
+  "chatAbot.exe"
+  "chatAbot.exe.manifest"
+  "chatAbot.pdb"
+  "libchatAbot.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/chatAbot.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- main.c
+++ main.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 
 int main() {
-    printf("Hello, World!\n");
+    printf("Let's talk\n");
+    char userimp[100];
+    gets(userimp);
+    printf("%s to you too. What's your name?", userimp);
     return 0;
 }
